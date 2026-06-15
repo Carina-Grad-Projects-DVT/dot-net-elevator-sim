46d9080253d31c99e2446dc866380a3b5b5a6adb
Carina Diesel <cdiesel@dvtsoftware.com>
2026-06-15 11:15:09 +0200

    feat: Added Elevator Factory

---
+++ ElevatorSim.Application/Factories/ElevatorFactory.cs
@@ -0,0 +1,71 @@
+// ElevatorSim.Application/Factories/ElevatorFactory.cs
+using ElevatorSim.Application.Models;
+using ElevatorSim.Domain.Entities;
+using ElevatorSim.Domain.Enums;
+using ElevatorSim.Domain.Interfaces;
+using ElevatorSim.Domain.ValueObjects;
+
+namespace ElevatorSim.Application.Factories;
+
+/// <summary>
+/// Factory for elevator creation.
+/// Only passenger elevators are implemented for now
+/// </summary>
+public sealed class ElevatorFactory : IElevatorFactory
+{
+    public IElevator Create(ElevatorConfiguration configuration)
+    {
+        ArgumentNullException.ThrowIfNull(configuration);
+
+        configuration.ValidateForCreation();
+
+        return configuration.Type switch
+        {
+            ElevatorType.Passenger => CreatePassengerElevator(configuration),
+            ElevatorType.Freight => throw new NotSupportedException(
+                "Freight elevator creation is not available yet."
+            ),
+            _ => throw new ArgumentOutOfRangeException(
+                nameof(configuration.Type),
+                configuration.Type,
+                "Unsupported elevator type."
+            ),
+        };
+    }
+
+    public IReadOnlyList<IElevator> CreateMany(IEnumerable<ElevatorConfiguration> configurations)
+    {
+        ArgumentNullException.ThrowIfNull(configurations);
+
+        var elevators = new List<IElevator>();
+        foreach (var configuration in configurations)
+        {
+            elevators.Add(Create(configuration));
+        }
+
+        return elevators;
+    }
+
+    private static IElevator CreatePassengerElevator(ElevatorConfiguration configuration)
+    {
+        if (configuration.MaximumPassengerCapacity is null)
+        {
+            throw new ArgumentException(
+                "MaximumPassengerCapacity is required",
+                nameof(configuration)
+            );
+        }
+
+        return new PassengerElevator(
+            id: new ElevatorId(configuration.Id),
+            startingFloor: FloorNumber.Create(
+                configuration.StartingFloor,
+                configuration.MinimumFloor,
+                configuration.MaximumFloor
+            ),
+            minimumFloor: configuration.MinimumFloor,
+            maximumFloor: configuration.MaximumFloor,
+            maximumPassengerCapacity: configuration.MaximumPassengerCapacity.Value
+        );
+    }
+}
---
+++ ElevatorSim.Application/Models/ElevatorConfiguration.cs
@@ -0,0 +1,61 @@
+using ElevatorSim.Domain.Enums;
+
+namespace ElevatorSim.Application.Models;
+
+/// <summary>
+/// Configuration used by the factory to create an elevator instance.
+/// </summary>
+public sealed record ElevatorConfiguration
+{
+    public required int Id { get; init; }
+    public required ElevatorType Type { get; init; }
+    public required int MinimumFloor { get; init; }
+    public required int MaximumFloor { get; init; }
+    public required int StartingFloor { get; init; }
+
+    // For Passengers
+    public int? MaximumPassengerCapacity { get; init; }
+
+    // For Freight (Unused for now)
+    public decimal? MaximumFreightLoadKg { get; init; }
+
+    public void ElevatorCreationValidation()
+    {
+        if (Id < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(Id),
+                "Elevator id must be greater than 0."
+            );
+        }
+
+        if (MinimumFloor > MaximumFloor)
+        {
+            throw new ArgumentException("MinimumFloor cannot be greater than MaximumFloor.");
+        }
+
+        if (StartingFloor < MinimumFloor || StartingFloor > MaximumFloor)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(StartingFloor),
+                $"StartingFloor must be between {MinimumFloor} and {MaximumFloor}."
+            );
+        }
+
+        if (Type == ElevatorType.Passenger)
+        {
+            if (MaximumPassengerCapacity is null || MaximumPassengerCapacity.Value < 1)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(MaximumPassengerCapacity),
+                    "MaximumPassengerCapacity must be set and greater than 0 passengers"
+                );
+            }
+        }
+
+        if (Type == ElevatorType.Freight)
+        {
+            throw new NotSupportedException("Freight elevator creation is not implemented yet.");
+        }
+    }
+}
